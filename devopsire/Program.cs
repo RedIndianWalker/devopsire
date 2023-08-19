b1c103194414febb9e344eae7e6c062f4b5b7be2
RedIndianWalker <redindianwalker@gmail.com>
2023-08-19 14:53:40 +0530

    Add project files.

---
+++ devopsire/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace devopsire
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
